4f93cc88529560f121430b9b42b679e82a916cf1
imaneboho2159 <imaboh21@gmail.com>
2025-03-05 13:58:32 +0100

    add class OffreEmploiModel

--- src/main/java/OffreEmploi/Model/OffreEmploiModel.java
+++ src/main/java/OffreEmploi/Model/OffreEmploiModel.java
@@ -1,46 +1,32 @@
 package OffreEmploi.Model;
 
+import Recruteur.MODEL.Recruteurs;
+
+import java.time.LocalDateTime;
+
 public class OffreEmploiModel {
-    private String nom;
-    private String description;
-    private String adresse;
 
-    public OffreEmploiModel(String nom, String description, String adresse) {
-        this.nom = nom;
-        this.description = description;
-        this.adresse = adresse;
-    }
+        private int id;
+        private String title;
+        private String description;
+        private Recruteurs recruteurs;
+        private LocalDateTime createdAt;
 
-    public String getNom() {
-        return nom;
-    }
+        public OffreEmploiModel(String title, String description, Recruteurs recruteurs ) {
+            this.title = title;
+            this.description = description;
+            this.recruteurs =recruteurs ;
+            this.createdAt = LocalDateTime.now();
+        }
 
-    public void setNom(String nom) {
-        this.nom = nom;
+        // Getters and Setters
+        public int getId() { return id; }
+        public void setId(int id) { this.id = id; }
+        public String getTitle() { return title; }
+        public void setTitle(String title) { this.title = title; }
+        public String getDescription() { return description; }
+        public void setDescription(String description) { this.description = description; }
+        public Recruteurs getRecruiter() { return recruteurs; }
+        public void setRecruiter(Recruteurs recruiter) { this.recruteurs = recruiter; }
+        public LocalDateTime getCreatedAt() { return createdAt; }
     }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getAdresse() {
-        return adresse;
-    }
-
-    public void setAdresse(String adresse) {
-        this.adresse = adresse;
-    }
-
-    @Override
-    public String toString() {
-        return "OffreEmploiModel{" +
-                "nom='" + nom + '\'' +
-                ", description='" + description + '\'' +
-                ", adresse='" + adresse + '\'' +
-                '}';
-    }
-}
